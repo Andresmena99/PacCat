22fcb51176d4aef89a5c0929b7b786da21457278
Andresmena99 <andres.mena@estudiante.uam.es>
2019-10-21 19:45:36 +0200

    DIA 1

--- rango/models.py
+++ rango/models.py
@@ -1,33 +1,76 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import slugify
+import datetime
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
-    likes = models.IntegerField(default=0)
+class User(models.Model):
+    name = models.CharField(max_length=128)
+    password = models.CharField(max_length=128)
     slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
-    class Meta:
-        verbose_name_plural = 'categories'
+    def __str__(self):
+        return self.name
+
+class GameStatus(Enum):
+    CREATED = 0
+    ACTIVE = 1
+    FINISHED = 2
+
+    @class_method
+    def get_values(cls):
+        return (
+            (cls.CREATED, 'Created'),
+            (cls.ACTIVE, 'Active'),
+            (cls.FINISHED, 'Finished'))
+
+
+class Game(models.Model):
+    cat_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mouse_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    cat1 = models.IntegerField(blank=False)
+    cat2 = models.IntegerField(blank=False)
+    cat3 = models.IntegerField(blank=False)
+    cat4 = models.IntegerField(blank=False)
+    mouse = models.IntegerField(blank=False)
+
+    def save(self, *args, **kwargs):
+        if 0<=cat1<=63 and 0<=cat2<=63 and 0<=cat3<=63 and 0<=cat4<=63:
+            super(Game, self).save(*args, **kwargs)
+        else:
+            #REVISAR
+            raise ValidationError("Casillas no válidas")
+
+
+    #REVISAR
+    status = models.CharField(choices = GameStatus.get_values(), status = GameStatus, blank=False)
 
     def __str__(self):
         return self.name
 
 
-class Page(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
+
+class Move(models.Model):
+    origin = models.IntegerField(blank=False)
+    target = models.IntegerField(blank=False)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    player = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default = datetime.now, blank=False)
 
     def __str__(self):
-        return self.title
+        return self.name
+
+
+
+
+
+
+
+
 
 
 class UserProfile(models.Model):
