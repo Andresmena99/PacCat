0c221336ab5182f3e05a227e93a51f9edac0398b
eric.hgh <eric.morales.s@gmail.com>
2019-11-12 01:15:35 +0100

    Metida toda la parte nueva de views y eso

--- logic/urls.py
+++ logic/urls.py
@@ -3,6 +3,16 @@ from logic import views
 # from django.conf.urls import url
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
     path('', views.index, name='landing'),
+    path('index/', views.index, name='index'),
+    path('login/', views.login_service, name='login'),
+    path('logout/', views.logout_service, name='logout'),
+    path('signup/', views.signup_service, name='signup'),
+    path('counter/', views.counter_service, name='counter'),
+    path('create_game/', views.create_game_service, name='create_game'),
+    path('join_game/', views.join_game_service, name='join_game'),
+    path('select_game/', views.select_game_service, name='select_game'),
+    path('play/', views.show_game_service, name='show_game'),
+    #path('move/', views.move_service, name='move'),
+    # REVISAR este ultimo habra que añadirlo, pero aun no hace falta
 ]
--- logic/views.py
+++ logic/views.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden
 from django.shortcuts import render
 
+# REVISAR estos imports son copypaste de lo anterior, igual sobra alguno
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.urls import reverse
+
+
 #from datamodel import constants
+from datamodel.forms import UserForm
 
 
 def anonymous_required(f):
@@ -30,3 +41,67 @@ def index(request):
 
     # Render the response and send it back!
     return render(request, 'mouse_cat/index.html')
+
+
+# REVISAR a todos los siguientes les falta el codigo de dentro
+
+@anonymous_required
+def login_service(request):
+    # REVISAR esta copiado de la anterior practica, faltan cosas
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('mouse_cat:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'mouse_cat/login.html', {'user_form': UserForm()})
+
+
+@login_required
+def logout_service(request):
+    # REVISAR esta copiado de la anterior practica
+    logout(request)
+    return redirect(reverse('mouse_cat:index'))
+
+
+def signup_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/signup.html')
+
+
+def counter_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/counter.html')
+
+
+def create_game_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/new_game.html')
+
+
+def join_game_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/join_game.html')
+
+
+def select_game_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/select_game.html')
+
+
+def show_game_service(request):
+    # REVISAR
+    return render(request, 'mouse_cat/game.html')
+
+
