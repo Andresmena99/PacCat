c1666be2f1d2dbbe5362317a23193c5e667e0cb9
eric.hgh <eric.morales.s@gmail.com>
2019-11-17 00:34:13 +0100

    Control de contraseñas

--- logic/forms.py
+++ logic/forms.py
@@ -12,8 +12,7 @@
 
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.password_validation import validate_password, \
-    CommonPasswordValidator
+from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 
 from datamodel.models import UserProfile, Move
